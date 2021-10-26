43a9c36a2b479dac815d56e976560bb7e2da19cf
Admin <password>
2021-10-26 17:02:27 +0800

    add: readme file to GitHub

--- pyDemo_1.py
+++ pyDemo_1.py
@@ -1,31 +1,42 @@
-import collections
-from pprint import pprint
+import module.common as co
+import time
+from datetime import datetime
 
-course = collections.namedtuple('course', ['name', 'field', 'attendee', 'remote'])
 
-# all courses
-poop = course(name='poop', field="python", attendee=10, remote=False)
-bdpy = course(name='bdpy', field="python", attendee=15, remote=True)
-andbiz3 = course(name='andbiz', field='android', attendee=18, remote=True)
-pykt = course(name='pykt', field="python", attendee=9, remote=True)
-aiocv = course(name='aiocv', field="python", attendee=20, remote=False)
-courses = (poop, bdpy, andbiz3, pykt, aiocv)
-print("COURSE DATA >>", ''.join([str(value) for value in courses]))
+if __name__ == '__main__':
+    # print("foo:", co.foo(1, 2))
+    # print("bar:", co.bar(3, 4))
+    # print("bar_muti:", co.bar_muti(3, 4))
+    # print("bar_muti:", co.bar_muti(3, 4, 5))
+    # print("bar_muti:", co.bar_muti(3, 4, *[5, 6, 7]))  # *可以拆解collection為多個參數
 
-# filter
-# filter(lambda x: x.remote is True, courses) => filter object
-# tuple(filter object) => tuple list
-remoteCourses = tuple(filter(lambda x: x.remote is True, courses))
-print("REMOTE COURSE DATA >>")
-pprint(remoteCourses)
+    # co.print_muti('Will U marry me?', 'YES', 'NO', name='Cindy', sex='female')
+    # co.print_muti(fix3='NO', fix1='Will U marry me?', name='Cindy', sex='female', fix2='YES')  # 可以指定參數順序
 
-# pickup attendee >15
-print("REMOTE COURSE DATA THAT　attendee>15 >>")
-pprint(tuple(c for c in remoteCourses if c.attendee > 15))
+    # profile = {'name': 'Cindy', 'sex': "female", 'age': 20, 'hobby': "reading"}
+    # co.print_muti('Will U marry me?', 'YES', 'NO', **profile)  # **可以拆解collection為key=val之型態
 
-name_and_field = map(lambda x: {'name': x.name, 'field': x.field}, courses)
-pprint(name_and_field)
-pprint([c for c in name_and_field])
-pprint(tuple([c for c in name_and_field]))
-pprint(tuple(name_and_field))
+    # =============================================
+
+    now = datetime.now()
+    print(now)
+    print(repr(now))
+    print([now])
+    print([str(now)])
+
+    # =============================================
+
+    print(math.pi, math.e, math.sqrt(5))
+    # print(math.sqrt(-1))
+    print((-1) ** 0.5)
+    for _ in range(5):
+        print(random.randint(0, 5))
+
+    stores = ['7-11', 'fami', 'ok', 'hi-life']
+    for _ in range(5):
+        print(random.choice(stores))
+    cards = ['A', 'K', 'Q', 'J', '10']
+    for _ in range(10):
+        print(cards)
+        random.shuffle(cards)
 
